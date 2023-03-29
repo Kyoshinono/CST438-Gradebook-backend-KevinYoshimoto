e23611d102922bb20bee046d860858fac1a5fb52
Kevin <kyoshimoto@csumb.edu>
2023-03-28 20:23:41 -0700

    Added a check for the course object in the method receive. Added output to console in sendFinalGrades before.

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -49,7 +51,11 @@ public class RegistrationServiceMQ extends RegistrationService {
 		Enrollment e = new Enrollment();
 		e.setStudentName(enrollmentDTO.studentName);
 		e.setStudentEmail(enrollmentDTO.studentEmail);
-		e.setCourse(courseRepository.findById(enrollmentDTO.course_id).orElse(null));
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if(c==null) {
+			System.out.println("Course id not found.");
+		}
+		e.setCourse(c);
 		enrollmentRepository.save(e);
 		System.out.println("Added new enrollment, name: " + enrollmentDTO.studentName + ", email: "+ enrollmentDTO.studentEmail + ", course id: " + enrollmentDTO.course_id);
 	}
@@ -61,8 +67,9 @@ public class RegistrationServiceMQ extends RegistrationService {
 		//TODO  complete this method in homework 4
 		//similar to method in registration service, use rabbit template instead of resttemplate
 		//rabbit template convert send
+		System.out.println("Sending final grade to the registration service for " + course_id);
 		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
-		System.out.println("Message send to registration service for Grades for course id: " + course_id);
+		System.out.println("Message sent to registration service for Grades for course id: " + course_id);
 	}
 
 }
